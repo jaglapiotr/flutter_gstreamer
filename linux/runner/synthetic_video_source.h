73bf87bcfde8f035b89c4082482f167a35f56a15
jaglapiotr <pjagla@idexcorp.com>
2026-03-13 09:17:46 +0100

    cleanup

--- linux/runner/synthetic_video_source.h
+++ linux/runner/synthetic_video_source.h
@@ -18,8 +18,6 @@ struct _SyntheticTextureClass {
 
 struct SyntheticTexturePrivate {
     int64_t texture_id = 0;
-    int32_t video_width = 0;
-    int32_t video_height = 0;
     FrameData* last_processed_frame = nullptr;
     FrameData* next_frame = nullptr;
 };
@@ -55,10 +53,8 @@ static gboolean synthetic_texture_copy_pixels(FlPixelBufferTexture* texture,
     }
 
     *out_buffer = synthetic_texture_private->last_processed_frame->map.data;
-    // *width = synthetic_texture_private->video_width;
-    // *height = synthetic_texture_private->video_height;
-    *width = 640;
-    *height = 360;
+    *width = 1280;
+    *height = 720;
 
     return TRUE;
 }
@@ -79,12 +75,9 @@ public:
             gst_init(nullptr, nullptr);
         }
 
-        width_ = 1280;
-        height_ = 720;
-
         std::string pipeline_str = 
             "videotestsrc ! "
-            "video/x-raw,width=640,height=360,framerate=30/1 ! "
+            "video/x-raw,width=1280,height=720,framerate=30/1 ! "
             "videoconvert ! "
             "video/x-raw,format=BGRA ! "
             "appsink name=sink emit-signals=true sync=true";
@@ -98,19 +91,13 @@ public:
             g_error_free(error);
         }
 
-
         synthetic_texture_ = synthetic_texture_new();
 
         FL_PIXEL_BUFFER_TEXTURE_GET_CLASS(synthetic_texture_)->copy_pixels = synthetic_texture_copy_pixels;
         fl_texture_registrar_register_texture(registrar_, FL_TEXTURE(synthetic_texture_));
 
-
-
-        // auto synthetic_texture_private = (SyntheticTexturePrivate*)synthetic_texture_get_instance_private(synthetic_texture_);
-        // synthetic_texture_private->texture_id = reinterpret_cast<int64_t>(FL_TEXTURE(synthetic_texture_));
-        // std::cout << "KANAPKA Texture ID: " << synthetic_texture_private->texture_id << std::endl;
-        synthetic_texture_private_ = (SyntheticTexturePrivate*)synthetic_texture_get_instance_private(synthetic_texture_);
-        synthetic_texture_private_->texture_id = reinterpret_cast<int64_t>(FL_TEXTURE(synthetic_texture_));
+        auto synthetic_texture_private = (SyntheticTexturePrivate*)synthetic_texture_get_instance_private(synthetic_texture_);
+        synthetic_texture_private->texture_id = reinterpret_cast<int64_t>(FL_TEXTURE(synthetic_texture_));
 
         GstElement* sink = gst_bin_get_by_name(GST_BIN(pipeline_), "sink");
         g_signal_connect(sink, "new-sample", G_CALLBACK(OnNewSample), this);
@@ -119,8 +106,6 @@ public:
         if(gst_element_set_state(pipeline_, GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE) {
             std::cerr << "!!! Couldn't set pipeline to playing state" << std::endl;
         }
-
-        (void)texture_id_;
     }
 
     int64_t texture_id() const {
@@ -146,23 +131,13 @@ private:
             if(buffer != NULL) {
                 gst_buffer_map(buffer, &frame->map, GST_MAP_READ);
 
-                // GstVideoInfo info;
-                // GstCaps* sampleCaps = gst_sample_get_caps(sample);
-                // gst_video_info_from_caps(&info, sampleCaps);
-
                 auto synthetic_texture_private = (SyntheticTexturePrivate*)synthetic_texture_get_instance_private(self->synthetic_texture_);
-                // synthetic_texture_private->buffer = (uint8_t*)frame.data;
-                // synthetic_texture_private->video_width = info.width;
-                // synthetic_texture_private->video_height = info.height;
                 if(synthetic_texture_private->next_frame) {
                     CleanupFrame(synthetic_texture_private->next_frame);
                 }
-                // synthetic_texture_private->video_width = info.width;
-                // synthetic_texture_private->video_height = info.height;
                 synthetic_texture_private->next_frame = frame;
                 fl_texture_registrar_mark_texture_frame_available(self->registrar_, FL_TEXTURE(self->synthetic_texture_));
 
-                // gst_caps_unref(sampleCaps);
             } else {
                 delete frame;
                 gst_sample_unref(sample);
@@ -174,13 +149,6 @@ private:
 
 
     GstElement* pipeline_ = nullptr;
-    int width_ = 0;
-    int height_ = 0;
     FlTextureRegistrar* registrar_ = nullptr;
-    // FlTexture* texture_ = nullptr;
-    // FlPixelBufferTexture* pixelBuffer_ = nullptr;
-    SyntheticTexturePrivate* synthetic_texture_private_ = nullptr;
     SyntheticTexture* synthetic_texture_ = nullptr;
-    int64_t texture_id_ = -1;
-
 };
