431392caf07d2ac9d1521f0fafb06e5755328922
Piotr Jagła <pjagla@idexcorp.com>
2026-08-06 14:21:24 +0200

    fix compilation

--- ios/Runner/GStreamerVideoTexture.h
+++ ios/Runner/GStreamerVideoTexture.h
@@ -2,11 +2,14 @@
 
 #import <Flutter/Flutter.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 // Thin Objective-C shim exposing SyntheticVideoSource (synthetic_video_source.h/.cpp, plain
 // C++) through the FlutterTexture protocol. Kept as a plain Objective-C interface (no C++
 // types) so it can be imported from Swift via Runner-Bridging-Header.h.
 @interface GStreamerVideoTexture : NSObject <FlutterTexture>
 
+// Marked nonnull (via NS_ASSUME_NONNULL) so Swift imports this as a non-failable init.
 - (instancetype)initWithTextureRegistry:(id<FlutterTextureRegistry>)registry
                                    width:(int)width
                                   height:(int)height;
@@ -17,3 +20,5 @@
 @property(nonatomic, readonly) int64_t textureId;
 
 @end
+
+NS_ASSUME_NONNULL_END
